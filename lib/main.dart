13a773a143e6f6b4a5d5fb778415fbfecbbd656b
Elif İrem Keskin <elifirem2006@gmail.com>
2025-04-28 12:54:23 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profil Kartı',
+      home: Scaffold(
+        backgroundColor: Colors.grey[200],
+        body: Center(
+          child: ProfileCard(),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          width: 300,
+          height: 200,
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10,
+                offset: const Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              SizedBox(height: 20), // profil fotoğrafı için boşluk
+              Text(
+                'Ahmet Yılmaz',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Mobil Geliştirici',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          top: -40,
+          left: 100,
+          child: CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/profile.jpg'),
+          ),
+        ),
+      ],
+    );
+  }
+}
